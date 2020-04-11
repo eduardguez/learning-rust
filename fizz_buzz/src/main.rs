c09e58a211dd67d910b5cc8507f10ecf510006c2
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-04-11 13:47:23 -0500

    Entry line is read as limit

--- fizz_buzz/src/main.rs
+++ fizz_buzz/src/main.rs
@@ -1,5 +1,16 @@
+use std::io;
+
 fn main() {
-  fizzbuzz_to(20);
+  loop {
+    let n = read_number();
+    match n.parse::<u32>() {
+      Ok(n) => {
+        fizzbuzz_to(n);
+        break;
+      }
+      Err(..) => continue,
+    };
+  }
 }
 
 fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
@@ -25,4 +36,11 @@ fn fizzbuzz_to(n: u32) {
   for n in 1..n + 1 {
     fizzbuzz(n)
   }
+}
+
+fn read_number() -> String {
+  println!("Please input your limit: ");
+  let mut any_string = String::new();
+  io::stdin().read_line(&mut any_string).expect("Failed to read line");
+  any_string.trim().to_string()
 }
