41f9058792f59f7ed79cc91d3c7c7cda2d28c27c
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-08-18 22:44:51 -0500

    Add method for loading environment variables and rocket routes

--- api-rust/src/main.rs
+++ api-rust/src/main.rs
@@ -8,11 +8,28 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate diesel;
 
+use dotenv::dotenv;
+use std::env;
+
 mod db;
 mod models;
 mod routes;
 mod schema;
 
+fn rocket() -> rocket::Rocket {
+  dotenv().ok();
+
+  let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+
+  let pool = db::init_pool(database_url);
+  rocket::ignite()
+    .manage(pool)
+    .mount(
+      "/api/v1/",
+      routes![],
+    )
+}
+
 fn main() {
-  rocket::ignite().launch();
+  rocket().launch();
 }
