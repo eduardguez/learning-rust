34db123e0cd29554ef15b02bc61752e79bfbe1e6
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-08-18 21:59:51 -0500

    Add database module to manage Postgres connections to Diesel

---
+++ api-rust/src/db.rs
@@ -0,0 +1,10 @@
+use r2d2_diesel::ConnectionManager;
+use diesel::pg::PgConnection;
+use r2d2::{ Pool };
+
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+
+pub fn init_pool(db_url: String) -> PgPool {
+  let connection_manager = ConnectionManager::<PgConnection>::new(db_url);
+  Pool::new(connection_manager).expect("failed to create connection pool")
+}
