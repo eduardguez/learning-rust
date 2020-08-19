bba199eab95558da5d2f419bda3647b72cd58297
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-08-18 22:13:50 -0500

    Add structure that wraps Postgres connection

--- api-rust/src/db.rs
+++ api-rust/src/db.rs
@@ -1,6 +1,6 @@
 use r2d2_diesel::ConnectionManager;
 use diesel::pg::PgConnection;
-use r2d2::{ Pool };
+use r2d2::{ Pool, PooledConnection };
 
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
 
@@ -8,3 +8,5 @@ pub fn init_pool(db_url: String) -> PgPool {
   let connection_manager = ConnectionManager::<PgConnection>::new(db_url);
   Pool::new(connection_manager).expect("failed to create connection pool")
 }
+
+pub struct Conn(pub PooledConnection<ConnectionManager<PgConnection>>);
