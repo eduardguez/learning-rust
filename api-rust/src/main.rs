35b90c9481da233a00153e0cc77418558da409d0
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-08-18 21:46:39 -0500

    Remove rocket routes from the main.rs

--- api-rust/src/main.rs
+++ api-rust/src/main.rs
@@ -5,56 +5,14 @@
 extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
+#[macro_use]
+extern crate diesel;
 
-use rocket_contrib::json::{Json, JsonValue};
-
-mod user;
-use user::{User};
-
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-  format!("Hi, I'm {} and I am {} years old!", name, age)
-}
-
-#[post("/users", data = "<user>")]
-fn post_user(user: Json<User>) -> Json<User> {
-  user
-}
-
-#[put("/users/<id>", data= "<user>")]
-fn put_user(id: i32, user: Json<User>) -> Json<User> {
-  user
-}
-
-#[delete("/users/<id>")]
-fn delete_user(id: i32) -> JsonValue {
-  json!({
-    "id": id,
-    "status": "ok",
-  })
-}
-
-#[get("/users")]
-fn get_users() -> JsonValue {
-  json!({
-    "name": "eduardo",
-    "username": "eduardgz",
-    "email": "eduardoperaltardgz@gmail.com",
-    "hash": "c79845bf39f29ad8b54dfe23a3918cad"
-  })
-}
+mod db;
+mod models;
+mod routes;
+mod schema;
 
 fn main() {
-  rocket::ignite()
-    .mount(
-      "/api/v1",
-      routes![
-        hello,
-        post_user,
-        put_user,
-        delete_user,
-        get_users,
-      ]
-    )
-    .launch();
+  rocket::ignite().launch();
 }
