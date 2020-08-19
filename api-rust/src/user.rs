26bb093d7098c1b62fa9aea23d6671d0fde02907
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-08-18 22:58:24 -0500

    Implements user structure to get all users from the database

--- api-rust/src/user.rs
+++ api-rust/src/user.rs
@@ -1,10 +1,25 @@
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use crate::schema::users;
+use crate::schema::users::dsl::users as all_users;
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Queryable, Serialize, Deserialize)]
 pub struct User {
   pub id: i32, 
   pub username: String,
   pub email: String,
   pub hash: String,
+}
+
+impl User {
+
+  pub fn get_all_users(conn: &PgConnection) -> Vec<User> {
+    all_users
+      .order(users::id.desc())
+      .load::<User>(conn)
+      .expect("error!")
+  }
+
 }
