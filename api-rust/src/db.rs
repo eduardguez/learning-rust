4b35516fd6ae6325fc686507048399957ab85baf
eduardo-peralta <eduardoperaltardgz@gmail.com>
2020-08-18 22:25:27 -0500

    Add Postgres connection request guard

--- api-rust/src/db.rs
+++ api-rust/src/db.rs
@@ -1,6 +1,9 @@
+use rocket::request::{ self, FromRequest };
+use rocket::{ Outcome, Request, State };
+use r2d2::{ Pool, PooledConnection };
 use r2d2_diesel::ConnectionManager;
 use diesel::pg::PgConnection;
-use r2d2::{ Pool, PooledConnection };
+use rocket::http::Status;
 
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
 
@@ -10,3 +13,15 @@ pub fn init_pool(db_url: String) -> PgPool {
 }
 
 pub struct Conn(pub PooledConnection<ConnectionManager<PgConnection>>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for Conn {
+  type Error = ();
+
+  fn from_request(request: &'a Request<'r>) -> request::Outcome<Conn, ()> {
+    let pool = request.guard::<State<PgPool>>()?;
+    match pool.get() {
+      Ok(conn) => Outcome::Success(Conn(conn)),
+      Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+    }
+  }
+}
